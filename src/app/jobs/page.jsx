310e69c4b924be75a347abf8a2d0e78f3967aa1e
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-09-09 19:41:41 +0100

    feat(search)

--- src/app/jobs/page.jsx
+++ src/app/jobs/page.jsx
@@ -13,7 +13,7 @@ import {
 import { LocateFixed, MapPin, Search, SlidersVertical } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import Filters from "@/components/Filters";
 
 export default function Page() {
@@ -66,86 +66,90 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex bg-white items-center p-2 ml-80 mr-96 rounded-xl mt-6 border w-search justify-center">
-        <Search />
+      <Suspense>
+        <div className="flex bg-white items-center p-2 ml-80 mr-96 rounded-xl mt-6 border w-search justify-center">
+          <Search />
 
-        <input
-          placeholder="Search by: Job title, Position, Keyword."
-          className="w-96"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <MapPin className="text-blue-500 m-2" />
-        <div className="flex">
           <input
-            placeholder="City, state or zip code"
-            className="w-80"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Search by: Job title, Position, Keyword."
+            className="w-96"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
-          <LocateFixed />
+          <MapPin className="text-blue-500 m-2" />
+          <div className="flex">
+            <input
+              placeholder="City, state or zip code"
+              className="w-80"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <LocateFixed />
+          </div>
+          <Filters />
+          <button
+            className="bg-blue-500 p-3 ml-4 rounded text-white"
+            onClick={findJob}
+          >
+            Find Job
+          </button>
         </div>
-        <Filters />
-        <button
-          className="bg-blue-500 p-3 ml-4 rounded text-white"
-          onClick={findJob}
-        >
-          Find Job
-        </button>
-      </div>
-      <div className="flex flex-wrap gap-5 justify-center mt-10">
-        {data?.jobs.map((job) => (
-          <JobCards key={job._id} job={job} />
-        ))}
-        {data?.jobs.length === 0 && <div>Job not found</div>}
-      </div>
-
-      <Pagination className="m-5">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious
-              href={`/jobs?page=${
-                parseInt(currentPage) - 1
-              }&title=${title}&location=${location}`}
-              aria-disabled={currentPage <= 1}
-              className={
-                currentPage <= 1 ? "pointer-events-none opacity-50" : undefined
-              }
-            />
-          </PaginationItem>
-
-          {Array.from({ length: data?.NumberOfPage }).map((_, page) => (
-            <PaginationItem key={page}>
-              <PaginationLink
-                isActive={page + 1 == currentPage}
-                className={cn(
-                  "rounded-full",
-                  page + 1 == currentPage && " bg-[#0A65CC] text-white"
-                )}
-                href={`/jobs?page=${
-                  page + 1
-                }&title=${title}&location=${location}`}
-              >
-                {page + 1}
-              </PaginationLink>
-            </PaginationItem>
+        <div className="flex flex-wrap gap-5 justify-center mt-10">
+          {data?.jobs.map((job) => (
+            <JobCards key={job._id} job={job} />
           ))}
+          {data?.jobs.length === 0 && <div>Job not found</div>}
+        </div>
 
-          <PaginationItem>
-            <PaginationNext
-              href={`/jobs?page=${
-                parseInt(currentPage) + 1
-              }&title=${title}&location=${location}`}
-              aria-disabled={currentPage > data?.NumberOfPage - 1}
-              className={
-                currentPage > data?.NumberOfPage - 1
-                  ? "pointer-events-none opacity-50"
-                  : undefined
-              }
-            />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+        <Pagination className="m-5">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                href={`/jobs?page=${
+                  parseInt(currentPage) - 1
+                }&title=${title}&location=${location}`}
+                aria-disabled={currentPage <= 1}
+                className={
+                  currentPage <= 1
+                    ? "pointer-events-none opacity-50"
+                    : undefined
+                }
+              />
+            </PaginationItem>
+
+            {Array.from({ length: data?.NumberOfPage }).map((_, page) => (
+              <PaginationItem key={page}>
+                <PaginationLink
+                  isActive={page + 1 == currentPage}
+                  className={cn(
+                    "rounded-full",
+                    page + 1 == currentPage && " bg-[#0A65CC] text-white"
+                  )}
+                  href={`/jobs?page=${
+                    page + 1
+                  }&title=${title}&location=${location}`}
+                >
+                  {page + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationNext
+                href={`/jobs?page=${
+                  parseInt(currentPage) + 1
+                }&title=${title}&location=${location}`}
+                aria-disabled={currentPage > data?.NumberOfPage - 1}
+                className={
+                  currentPage > data?.NumberOfPage - 1
+                    ? "pointer-events-none opacity-50"
+                    : undefined
+                }
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </Suspense>
     </>
   );
 }
